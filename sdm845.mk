b4184e73b12a151ce9f020c7530b6d24b764a148
SonalSingh18 <sonal.singh.19993@gmail.com>
2020-07-04 22:19:20 +0530

    sdm845-common: Import PocoParts
    Features:
    • Selinux Toggle
    • Toggle Usb Fast Charge
    
    Change-Id: I2947aa62b78b1150abde3484b7d3af5d97267f80
    Co-authored-by: StyloGey <stylogey@gmail.com>
    Co-authored-by: Rizky Benggolo <travarilo@gmail.com>
    Co-authored-by: Genkzsz11 <genkzsz11@gmail.com>
    Co-authored-by: smokey18 <kmboplays@gmail.com>
    Co-authored-by: ronaxdevil <pratabidya.007@gmail.com>
    Co-authored-by: SiddharthBharadwaj <45765901+SiddharthBharadwaj@users.noreply.github.com>
    Co-authored-by: Saurabh Charde <saurabhchardereal@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -130,6 +130,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
+    init.pocoparts.sh \
+    init.pocoparts.rc \
     ueventd.qcom.rc
 
 # Context Hub
@@ -139,7 +141,8 @@ PRODUCT_PACKAGES += \
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
-    XiaomiParts
+    XiaomiParts \
+    PocoParts
 
 # Display
 PRODUCT_PACKAGES += \
