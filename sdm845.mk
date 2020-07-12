1f8f2637e6e7bf97447d28048cd574d2798f0acf
SonalSingh18 <sonal.singh.19993@gmail.com>
2020-07-12 16:25:45 +0530

    sdm845-common: Add MotoDolby
     * Also build ldac libs
    
    Co-authored-by: Saikiran <bjsaikiran@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -108,6 +108,8 @@ PRODUCT_PACKAGES += \
     BluetoothQti \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    libldacBT_enc \
+    libldacBT_abr \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
@@ -223,6 +225,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
