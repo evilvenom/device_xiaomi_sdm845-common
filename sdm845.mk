8d09850e7d0f5778f2a6a8ddee8d2ebc835d2082
EvilVenøm <ktanay98@gmail.com>
2021-10-12 04:47:22 -0400

    sdm845-common: drop livedisplay

--- sdm845.mk
+++ sdm845.mk
@@ -207,8 +207,8 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm845
 
 # LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_PACKAGES += \
