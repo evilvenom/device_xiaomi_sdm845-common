309007624838a70d4a4c65a124b2b677f77ea171
EvilVenøm <ktanay98@gmail.com>
2021-10-12 04:29:34 -0400

    sdm845-common: don't build Wfd

--- sdm845.mk
+++ sdm845.mk
@@ -353,9 +353,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # WiFi Display
-PRODUCT_PACKAGES += \
-    libnl \
-    libwfdaac_vendor
+#PRODUCT_PACKAGES += \
+#    libnl \
+#    libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
