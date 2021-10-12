1b318af834aafc44ad95a8acb467e2cc6332897a
EvilVenøm <ktanay98@gmail.com>
2021-10-12 04:55:03 -0400

    sdm845-common: Drop org.ifaa.android.manager

--- sdm845.mk
+++ sdm845.mk
@@ -187,11 +187,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
+#PRODUCT_PACKAGES += \
+#    org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+#PRODUCT_BOOT_JARS += \
+#    org.ifaa.android.manager
 
 # Input
 PRODUCT_COPY_FILES += \
